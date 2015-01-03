f9af6be7d35ce000f47dd6cb95176b79efa2205a
Jason A Smith <jas7553@gmail.com>
2015-01-03 13:12:54 -0800

    Replace ViewController with DefragViewController

--- satisfaction/DefragViewController.swift
+++ satisfaction/DefragViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DefragViewController.swift
 //  satisfaction
 //
 //  Created by Jason A Smith on 1/3/15.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class DefragViewController: UICollectionViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-

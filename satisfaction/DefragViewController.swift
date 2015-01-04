9875d0d08699f07ee5e9b260388be86128249819
Jason A Smith <jas7553@gmail.com>
2015-01-03 17:22:57 -0800

    Create DefragViewController with grid of random colors
    DefragViewController contains one subview, a UICollectionView. The grid fills
    ~75% of an iPhone 6 screen, with random amounts of red, green, and blue cells.
    
    There's no logic for sorting or moving the cells yet. The cells are static.

--- satisfaction/DefragViewController.swift
+++ satisfaction/DefragViewController.swift
@@ -8,16 +8,62 @@
 
 import UIKit
 
-class DefragViewController: UICollectionViewController {
+class DefragViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private let redColor = UIColor(red: 0.8902, green: 0.2549, blue: 0.2667, alpha: 1)
+    private let greenColor = UIColor(red: 0.2431, green: 0.7882, blue: 0.3216, alpha: 1)
+    private let blueColor = UIColor(red: 0.2863, green: 0.4627, blue: 0.8863, alpha: 1)
+    
+    private let cellCount = 25 * 30;
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        collectionView.contentInset = UIEdgeInsets(top:10, left: 10, bottom: 10, right: 10)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: UICollectionViewDataSource
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellCount
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("DefragCell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        switch Int(arc4random_uniform(3)) {
+        case 0:
+            cell.backgroundColor = blueColor
+        case 1:
+            cell.backgroundColor = greenColor
+        default:
+            cell.backgroundColor = redColor
+        }
+        
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(10, 10)
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+        return 4
     }
     
 }
